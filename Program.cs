e7b1316d46654dfeeb1a171ea9f1b3431cf30456
THirvola <thirvola@gmail.com>
2024-07-08 20:55:01 +0300

    Github workflow tracking via REST API

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-//using CliWrap;
-//using LibGit2Sharp;
-//using LibGit2Sharp.Handlers;
-using TatunBlazorApp.Components;
+using CICD_Demo.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,66 +23,5 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
-//initializing the repository
-/*
-Repository repo = new Repository("path");
-repo.Index.Add("newFile.txt");
-Commands.Stage(repo, "newFile.txt");
-Signature sign = new Signature("name", "email", DateTimeOffset.Now);
-repo.Commit("Automated commit by the app", sign, sign);
-
-//Remote remote = repo.Network.Remotes.Add("origin", "https://github.com/THirvola/AzureCICDProject.git");
-
-if (repo.Head.Commits != null && repo.Head.Commits.Any())
-{
-    Remote remote = repo.Network.Remotes["origin"];
-
-    var pushOptions = new PushOptions
-    {
-        CredentialsProvider = new CredentialsHandler(
-        (url, usernameFromUrl, types) =>
-            new UsernamePasswordCredentials()
-            {
-                Username = "username",
-                Password = "pass",
-            }
-    ),
-    };
-    repo.Network.Push(remote, "refs/heads/master", pushOptions);
-}
-System.Console.WriteLine(repo.Head.Tip.Message);
-*/
-//cliwrap test
-//todo: test if cliwrap works on local machine
-/*
-CommandResult result = await Cli.Wrap("git").WithArguments("add --all").ExecuteAsync();
-if (result.IsSuccess)
-{
-    result = await Cli.Wrap("git").WithArguments(["commit", "-m", "Automated commit by the application"]).WithValidation(CommandResultValidation.None).ExecuteAsync();
-    if (result.IsSuccess || result.ExitCode == 141)
-    {
-        System.Console.WriteLine("successfull commit: " + result.IsSuccess);
-
-        System.Console.WriteLine("exit code: " + result.ExitCode);
-        
-        result = await Cli.Wrap("git").WithArguments("push origin master").ExecuteAsync();
-        if (result.IsSuccess)
-        {
-            System.Console.WriteLine("Successfully added, committed and pushed to repository");
-        }
-
-    }
-    else
-    {
-        System.Console.WriteLine("failed commit");
-
-        System.Console.WriteLine("exit code: " + result.ExitCode);
-    }
-}
-*/
-
-
-//todo: if works on local, try to get git on azure machine
 
 app.Run();
